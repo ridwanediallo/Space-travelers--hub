20e20c0171f4b7e0d8f5c0288e16db7941e20166
ridwan <ridwanediallo99@gmail.com>
2022-05-16 23:52:53 +0000

    Add activeLink

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './NavBar.css'
+import './NavBar.css';
 import Logo from './planet.png';
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
+  const activeLink = ({ isActive }) => {
+    return {
+      color: isActive ? '#95bcf0' : '',
+      paddingBottom: isActive ? '0.25rem' : '',
+      borderBottom: isActive ? '3px solid #95bcf0' : '',
+    };
+  };
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
@@ -13,23 +20,47 @@ const NavBar = () => {
           <img className="logo" src={Logo} alt="logo" />
           <h2 className="ps-3 text-dark">Space Travelers Hub</h2>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"  className="text-dark"/>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className="text-dark"
+        />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#rockets">
-            <NavLink to="/" className="text-primary link" activeStyle="active">Rockets</NavLink>
+              <NavLink
+                style={activeLink}
+                to="/"
+                className="text-primary link"
+                activeStyle="active"
+              >
+                Rockets
+              </NavLink>
             </Nav.Link>
             <Nav.Link href="#missions">
-            <NavLink to="/missions" className="text-primary link" activeStyle="active">Missions</NavLink>
+              <NavLink
+                style={activeLink}
+                to="/missions"
+                className="text-primary link"
+                activeStyle="active"
+              >
+                Missions
+              </NavLink>
             </Nav.Link>
             <Nav.Link href="#profile">
-             <NavLink to="/profile" className="text-primary link" activeStyle="active">My Profile</NavLink>
+              <NavLink
+                style={activeLink}
+                to="/profile"
+                className="text-primary link"
+                activeStyle="active"
+              >
+                My Profile
+              </NavLink>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
-export default NavBar
+export default NavBar;
