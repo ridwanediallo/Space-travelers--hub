0a5b79200588aa0c174fd1e0e569a84233c1b411
ridwan <ridwanediallo99@gmail.com>
2022-05-17 15:33:19 +0000

    Add a title for every card

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
   return (
     <div className="container d-flex justify-content-center my-5">
-      <div className="card mission-card">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-        </ul>
+      <div>
+        <h2>My Missions</h2>
+        <div className="card mission-card">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
       </div>
-      <div className="card rocket-cart ms-5">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-        </ul>
+      <div>
+        <h2 className="ms-5">My Rockets</h2>
+        <div className="card rocket-cart ms-5">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Profile
+export default Profile;
