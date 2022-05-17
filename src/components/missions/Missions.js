4c11248753d18dbf1110bab80192c6afd33b792e
ridwan <ridwanediallo99@gmail.com>
2022-05-17 09:39:12 +0000

    Add Table set up

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Table} from 'react-bootstrap';
 
 const Missions = () => {
   return (
-    <div>Missions</div>
-  )
+    <Table striped bordered hover size="lg" className="container my-5">
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
 }
 
 export default Missions
