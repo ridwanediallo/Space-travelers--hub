2fa52a4cf400a4b2c105c8d408d70ab980545706
ridwan <ridwanediallo99@gmail.com>
2022-05-17 13:45:22 +0000

    Finish table styling

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -3,7 +3,14 @@ import {Table} from 'react-bootstrap';
 
 const Missions = () => {
   return (
-    <Table striped bordered hover size="lg" className="container my-5">
+    <Table
+      striped
+      bordered
+      // hover
+      size="lg"
+      className="container my-5"
+      responsive="lg"
+    >
       <thead>
         <tr>
           <th>Mission</th>
@@ -15,17 +22,26 @@ const Missions = () => {
         <tr>
           <td>Mark</td>
           <td>Otto</td>
-          <td>@mdo</td>
+          <td>
+            <div className="btns">
+              <tr>
+                <button type="button" className="btn btn-secondary ms-3">
+                  NOT A MEMBER
+                </button>
+                <button type="button" className="btn btn-outline-dark ms-3">Join Mission</button>
+              </tr>
+            </div>
+          </td>
         </tr>
         <tr>
           <td>Jacob</td>
           <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
+          <div className="btns">
+            <tr>
+              <button type="button" className="btn btn-info ms-3">NOT A MEMBER</button>
+              <button type="button" className="btn btn-outline-danger ms-3">Join Mission</button>
+            </tr>
+          </div>
         </tr>
       </tbody>
     </Table>
