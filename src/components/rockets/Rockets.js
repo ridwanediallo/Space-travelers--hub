72e24ee61e8c0026d060ad230ac1cfbe0e9673d9
sainingo <nickmentor421@gmail.com>
2022-05-17 12:14:34 +0300

    Setup & initialized the env

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Row,Col, Container } from 'react-bootstrap';
+import rocket1 from './rocket1.png';
 
 const Rockets = () => {
+  const id = Date.now();
   return (
-    <div>Rockets</div>
+    <Container>
+    <Row>
+    <Col>
+      <img src={rocket1} alt="Image" />
+    </Col>
+    <Col >Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with</Col>
+    <Col>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with</Col>
+  </Row>
+  </Container>
   )
 }
 
