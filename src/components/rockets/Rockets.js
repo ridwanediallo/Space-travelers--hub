26df1ea9d71a88e3c58af1c201f0891aeefee1b0
sainingo <nickmentor421@gmail.com>
2022-05-17 13:00:11 +0300

    Add styling

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Row,Col, Container } from 'react-bootstrap';
+import { Col, Row,Button, Container } from 'react-bootstrap';
 import rocket1 from './rocket1.png';
+import './rockets.css';
 
 const Rockets = () => {
   const id = Date.now();
   return (
     <Container>
-    <Row>
-    <Col>
-      <img src={rocket1} alt="Image" />
-    </Col>
-    <Col >Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with</Col>
-    <Col>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with</Col>
-  </Row>
+   <div className='rocket-container'>
+   <div>
+      <img src={rocket1} className="rockets-image" alt="rocket image" />
+    </div>
+    <div>
+      <h2>Falcon 1</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Fusce ut placerat orci nulla pellentesque. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum. Mi bibendum neque egestas congue. 
+        Viverra maecenas accumsan lacus vel facilisis volutpat est. Malesuada nunc vel risus commodo. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas.
+         At erat pellentesque adipiscing commodo elit. Imperdiet sed euismod nisi porta. Enim facilisis gravida neque convallis a cras semper auctor.</p>
+         <Button variant="primary">Primary</Button>{' '}
+    </div>
+   </div>
   </Container>
   )
 }
