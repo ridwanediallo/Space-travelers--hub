6acdba182bfce45214f6275a07c807f5966e40dc
sainingo <nickmentor421@gmail.com>
2022-05-17 17:45:32 +0300

    Improve on styling

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -11,13 +11,12 @@ const Rockets = () => {
    <div>
       <img src={rocket1} className="rockets-image" alt="rocket image" />
     </div>
-    <div>
+    <div className='grid-row'>
       <h2>Falcon 1</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Fusce ut placerat orci nulla pellentesque. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum. Mi bibendum neque egestas congue. 
-        Viverra maecenas accumsan lacus vel facilisis volutpat est. Malesuada nunc vel risus commodo. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas.
-         At erat pellentesque adipiscing commodo elit. Imperdiet sed euismod nisi porta. Enim facilisis gravida neque convallis a cras semper auctor.</p>
-         <Button variant="primary">Primary</Button>{' '}
+        Viverra maecenas accumsan lacus vel facilisis volutpat est.</p>
+         <Button className='button' variant="primary">Primary</Button>{' '}
     </div>
    </div>
   </Container>
