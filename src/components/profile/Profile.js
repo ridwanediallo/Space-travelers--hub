a3172e46337fb165d8b95a0da9257453d2867b3d
ridwan <ridwanediallo99@gmail.com>
2022-05-17 15:20:42 +0000

    Add profile page structure

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
+    <div className="container d-flex justify-content-center my-5">
+      <div className="card mission-card">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+      </div>
+      <div className="card rocket-cart ms-5">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Profile
