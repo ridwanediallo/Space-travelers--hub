6330ae058e505f1adf6a03f377580f9d5dea84fa
sainingo <nickmentor421@gmail.com>
2022-05-18 10:58:38 +0300

    Fix eslint error

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -9,14 +9,18 @@ const Rockets = () => {
   const dispatch = useDispatch();
   const {posts, loading} = useSelector(state => state.post)
   useEffect(() => {
-    dispatch(getPosts())
-  }, [])
+      dispatch(getPosts())
+  }, [dispatch, getPosts])
+
+  if(loading) {
+    return <h2>Loading.....</h2>
+  }
   return (
     <Container>
       {posts.map((item) => (
          <div key={item.id} className='rocket-container'>
          <div>
-            <img src={item.flickr_images[1]} className="rockets-image" alt="rocket image" />
+            <img src={item.flickr_images[0]} className="rockets-image" alt="rocket image" />
           </div>
           <div className='grid-row'>
             <h2>{item.rocket_name}</h2>
