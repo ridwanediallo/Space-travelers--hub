68f3e3fcc991c150a6e3180656c870c8fdde5aa4
sainingo <nickmentor421@gmail.com>
2022-05-19 10:21:17 +0300

    Update reserve button func

--- src/redux/rockets/postSlice.js
+++ src/redux/rockets/postSlice.js
@@ -1,14 +1,31 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getPosts = createAsyncThunk('posts/getPosts', async () => {
-    return fetch('https://api.spacexdata.com/v3/rockets').then((response) => response.json());
-})
+   const response = await fetch('https://api.spacexdata.com/v3/rockets');
+   const rocketsData = await response.json();
+   const fetchedRockets = rocketsData.map((rocket) => ({
+       id: rocket.id,
+       rocketName: rocket.rocket_name,
+       description: rocket.description,
+       rocketImage: rocket.flickr_image[0],
+       canceled: false,
+   }));
+   return fetchedRockets;
+});
 
 const postSlice = createSlice({
     name: 'posts',
     initialState: {
         posts: [],
         loading: false,
+    },
+    reducers: {
+        updateRockets: (state, action) => state.map((rocket) => {
+            if(rocket.id === action.payload) {
+                return {...rocket, canceled: !rocket.canceled};
+            }
+            return rocket;
+        })
     },
 
     extraReducers: {
@@ -19,10 +36,9 @@ const postSlice = createSlice({
             state.loading = false;
             state.posts = action.payload;
         },
-        [getPosts.rejected]: (state, action) => {
-            state.loading = false;
-        },
+      
     },
 });
 
+export const {updateRockets} = postSlice.actions;
 export default postSlice.reducer
