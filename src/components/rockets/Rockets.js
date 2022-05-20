ef943da7b562c7deb8d5cdc3100b24d207b44eff
ridwan <ridwanediallo99@gmail.com>
2022-05-20 10:47:00 +0000

    Update cancel Reservation btn style

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -7,7 +7,7 @@ import { getPosts, updateRockets } from '../../redux/rockets/postSlice';
 const Rockets = () => {
   const dispatch = useDispatch();
   const allRockets = useSelector((state) => state.post);
-  
+
   useEffect(() => {
       if(!allRockets.length) dispatch(getPosts());
   }, [dispatch, getPosts])
@@ -18,33 +18,58 @@ const Rockets = () => {
   }
   return (
     <Container>
-      {allRockets.map((item) => (
-         !item.canceled ? (
-          <div key={item.id} className='rocket-container'>
-          <div>
-             <img src={item.rocket_image} className="rockets-image" alt="rocket image" />
-           </div>
-           <div className='grid-row'>
-             <h2>{item.rocket_name}</h2>
-             <p>{item.description}</p>
-                <Button id={item.id} onClick={handleRocketsReservation} className='button' variant="primary">Reserve Rockets</Button>{' '}
-           </div>
+      {allRockets.map((item) =>
+        !item.canceled ? (
+          <div key={item.id} className="rocket-container">
+            <div>
+              <img
+                src={item.rocket_image}
+                className="rockets-image"
+                alt="rocket image"
+              />
+            </div>
+            <div className="grid-row">
+              <h2>{item.rocket_name}</h2>
+              <p>{item.description}</p>
+              <Button
+                id={item.id}
+                onClick={handleRocketsReservation}
+                className="button"
+                variant="primary"
+              >
+                Reserve Rockets
+              </Button>{' '}
+            </div>
           </div>
-         ) : (
-          <div key={item.id} className='rocket-container'>
-          <div>
-             <img src={item.rocket_image} className="rockets-image" alt="rocket image" />
-           </div>
-           <div className='grid-row'>
-             <h2>{item.rocket_name}</h2>
-             <p><span className='reserve-span'>reserved</span>{item.description}</p>
-                <Button id={item.id} onClick={handleRocketsReservation} className='button' variant="info">Cancel Reservation</Button>{' '}
-           </div>
+        ) : (
+          <div key={item.id} className="rocket-container">
+            <div>
+              <img
+                src={item.rocket_image}
+                className="rockets-image"
+                alt="rocket image"
+              />
+            </div>
+            <div className="grid-row">
+              <h2>{item.rocket_name}</h2>
+              <p>
+                <span className="reserve-span">reserved</span>
+                {item.description}
+              </p>
+              <Button
+                id={item.id}
+                onClick={handleRocketsReservation}
+                className="button"
+                variant="white shadow border"
+              >
+                Cancel Reservation
+              </Button>{' '}
+            </div>
           </div>
-         )
-      ))}
-  </Container>
-  )
+        )
+      )}
+    </Container>
+  );
 }
 
 export default Rockets
