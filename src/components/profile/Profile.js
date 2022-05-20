4125d00afd0d7829cbda2087c3f870543d097a0e
ridwan <ridwanediallo99@gmail.com>
2022-05-20 11:01:34 +0000

    Fix reserRocket is undiffined

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-
 const Profile = () => {
   const joinMissions = useSelector((state) => state.mission);
-  const filteredMissions = joinMissions.filter((mission) => mission.canceled === true);
+  const reserveRockets = useSelector((state) => state.post);
+  const filteredMissions = joinMissions.filter(
+    (mission) => mission.canceled === true
+  );
+  const reservedRockets = reserveRockets.filter(
+    (rocket) => rocket.canceled === true
+  );
   console.log(filteredMissions);
   return (
     <div className="container d-flex justify-content-center my-5">
@@ -12,28 +17,35 @@ const Profile = () => {
         <h2>My Missions</h2>
         <div className="card mission-card">
           <ul className="list-group list-group-flush">
-           {filteredMissions.map((mission) => {
-        
-             const {id, name} = mission;
-             console.log(id);
-             console.log(name);
+            {filteredMissions.map((mission) => {
+              const { id, name } = mission;
+              console.log(id);
+              console.log(name);
               return (
-                <li key={id} className="list-group-item">{name}</li>
-              )
-           })}
+                <li key={id} className="list-group-item">
+                  {name}
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
       <div>
         <h2 className="ms-5">My Rockets</h2>
         <div className="card rocket-cart ms-5">
-          <ul className="list-group list-group-flush">  
-            {reservedRockets.length ? reservedRockets.map((rocketName) => { 
-            const {rocket_name, id} = rocketName;
-             return (
-              <li key={id} className="list-group-item">{rocket_name}</li>
-             )
-          }) : <p className='p-2 text-danger'>Please reserve a rocket</p>} 
+          <ul className="list-group list-group-flush">
+            {reservedRockets.length ? (
+              reservedRockets.map((rocketName) => {
+                const { rocket_name, id } = rocketName;
+                return (
+                  <li key={id} className="list-group-item">
+                    {rocket_name}
+                  </li>
+                );
+              })
+            ) : (
+              <p className="p-2 text-danger">Please reserve a rocket</p>
+            )}
           </ul>
         </div>
       </div>
